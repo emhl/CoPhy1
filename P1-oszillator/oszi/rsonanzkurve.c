503741584d60cebce591c5a42506827c769cf40a
emhl <emil@steinr.eu>
2022-05-20 10:49:09 +0200

    add dominiks files

---
+++ P1-oszillator/oszi/rsonanzkurve.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define H 0.01 // Zeitschritt
+#define Omega 1 // Frequenz
+#define T 500.0
+#define d 0.1
+#define dwt 0.01
+#define wmax 10
+#define EINSCHWINGZEIT 150
+
+    int main() {
+        double xn2, xn1, xn, fn;
+        //double wt = 1;
+        double t = 0;
+        double x0 = 0;
+        double v = 1.0;
+        double amplitude = 0;
+        
+
+        
+        xn1 = x0 + H * v;
+        t = t + H;
+
+        for(double wt = 0.; wt < wmax; wt = wt + dwt){
+            amplitude = 0;
+            for(t = 2*H; t < T; t += H){
+                fn = sin(wt * t);
+                xn2 = 1./(2. + d*H) *(2 * H*H * fn + 2. * xn1 * (2. - H*H*Omega*Omega ) + xn* (d*H - 2.));
+                xn = xn1;
+                xn1 = xn2;
+                if(t > EINSCHWINGZEIT){
+                    if(xn > amplitude){
+                        amplitude = xn;
+                    }
+                }
+            }
+            printf("%g %g\n", wt, amplitude);
+    }
+        return 0;
+    }
---
+++ P1-oszillator/oszi/verlet-schwing.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define T 30.0	// Laufzeit
+#define H 0.005	// Zeitschritt
+#define OMEGA2 1.	// Frequenz
+#define D .1 // Dämpfung
+#define X0 0. //Startwert x
+#define V0 1. //Startwert 
+
+
+int main() {
+    double t,xl=X0, x, v,xn,c, E;
+    x = xl + H*V0;
+    v = V0 - OMEGA2*H*x; // Euler metheode für ersten schritt
+    E = (v*v + OMEGA2*x*x)/2.0;
+	for (t = H; t <= T; t += H){
+        printf("%g %g %g %g\n",t,x,v,E);
+        xn = 1./(2.+ D*H) * (4*x - 2*x*H*H*OMEGA2 + xl *(D*H - 2));
+        v = (xn-xl)/(2*H);
+        xl = x;
+        x = xn;
+        E = (v*v + OMEGA2*x*x)/2.0;
+	}
+
+}
