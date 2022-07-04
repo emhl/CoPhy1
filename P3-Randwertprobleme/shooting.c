0840cdb07785fd4bd303fb7fc9248f2d3e19cbcc
emhl <emil@steinr.eu>
2022-07-04 16:53:57 +0200

    Erfolgreich daran gescheitert die funktion zu spigeln und eine gute testfunktion zu suchen

--- P3-Randwertprobleme/shooting.c
+++ P3-Randwertprobleme/shooting.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
 
 #define H 0.001
 #define XMAX 20.0
@@ -14,26 +16,47 @@ double k2(double E,double x) {
 	return x-2*E;
 }
 
-double numerov(double E,int save) {
-    double x, fnm1=0.0, fn=H, fnp1;
+double numerov(double E, int save) {
+    double x, fnm1=0.0, fn=H, fnp1, ret=0;
     double y,df;
     int i = 0;
 
     //rechte seite
     for(x=H;x <= XMAX;x += H) {
 		fnp1 = (H*H/12.0*(F(x+H)+10.0*F(x)+F(x-H)) + fn*(2.0-5.0/6.0*H*H*k2(E,x))-fnm1*(1.0+H*H/12.0*k2(E,x-H)))/(1.0+H*H/12.0*k2(E,x+H));
-        printf("%g %g\n",x,fn);
+        if (save ==1){
+            printf("%g %g\n",x,fn);
+        }
+
         fnm1=fn;
 	    fn=fnp1;
-    }
-
-    if (save == 1){
         psi[i++] = fn;
     }
 
+    
+    for(int x=N - 100 ;x < N;x++) {
+        // ret += fabs(psi[x]);
+        ret += psi[x];
+        
+    }
+
+    return ret;
 }
 
 int main(){
-    double E1 = 1.;
-    numerov(E1,1);
+    psi = (double *)malloc(2*(int)N*sizeof(double));
+    double E0=0,E1 = 2.,Ex,tmp;
+    // numerov(E1);
+    // printf("%g %g\n", numerov(E1),0.);
+    do {
+		Ex = (E1+E0)/2.0;
+		// bisection (non continous !)
+		tmp = numerov(Ex,0);
+	    if(tmp*numerov(E0,0)>0)
+			E0=Ex;
+	    else
+		    E1=Ex;
+	} while (fabs((E1-E0)/E0)>1.e-14);
+    numerov(Ex,1);
+
 }
