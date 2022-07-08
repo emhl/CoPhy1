7077605686420f8d70c04f543029feba8accc790
emhl <emil@steinr.eu>
2022-07-08 18:02:54 +0200

    Finish Project 3 RWP

--- P3-Randwertprobleme/numerov.c
+++ P3-Randwertprobleme/numerov.c
@@ -1,41 +1,53 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 #define H 0.001
 #define XMAX 20.0
-#define N (XMAX/H)
+#define N (XMAX / H)
 #define EPS0 1
 
+double F(double x) { return -x / (8 * M_PI * EPS0) * exp(-x); }
 
-double F(double x){
-    return -x/(8*M_PI*EPS0)*exp(-x);
+double k2(double x) { return 0.0; }
+
+double numerov(double x, double fnm1, double fn) {
+  return (H * H / 12.0 * (F(x + H) + 10.0 * F(x) + F(x - H)) +
+          fn * (2.0 - 5.0 / 6.0 * H * H * k2(x)) -
+          fnm1 * (1.0 + H * H / 12.0 * k2(x - H))) /
+         (1.0 + H * H / 12.0 * k2(x + H));
 }
 
-double k2(double x) {
-	return 0.0;
+double chi(double x) { // analytische lösung von chi(r)
+  return (-(1 + x / 2.) / (4. * M_PI * EPS0) * exp(-x) +
+          1 / (4. * M_PI * EPS0)) /
+         .08;
 }
 
 int main() {
-    double x, fnm1=0.0, fn=H, fnp1;
-    double y,steig;
+  double x, fnm1 = 0.0, fn = H, fnp1;
+  double y, steig;
 
-    //unkorrigiert
-    for(x=H;x <= XMAX;x += H) {
-		fnp1 = (H*H/12.0*(F(x+H)+10.0*F(x)+F(x-H)) + fn*(2.0-5.0/6.0*H*H*k2(x))-fnm1*(1.0+H*H/12.0*k2(x-H)))/(1.0+H*H/12.0*k2(x+H));
-        // printf("%g %g\n",x,fn);
-        fnm1=fn;
-	    fn=fnp1;
-    }
-    steig = (fn-fnm1)/H;
+  // unkorrigiert
+  for (x = H; x <= XMAX; x += H) {
+    fnp1 = numerov(x, fnm1, fn);
+    fnm1 = fn; // letzter und aktueller wert werden zwischengespeichert
+    fn = fnp1;
+  }
+  steig = (fn - fnm1) /
+          H; // berechnung des linearen terms aus den letzten zwei werten
 
-      // nochmal laufen lassen während man den linearen term abzieht
-    fnm1=0.0;
-    fn=H;
-    for(x=H;x <= XMAX;x += H) {
-		fnp1 = (H*H/12.0*(F(x+H)+10.0*F(x)+F(x-H)) + fn*(2.0-5.0/6.0*H*H*k2(x))-fnm1*(1.0+H*H/12.0*k2(x-H)))/(1.0+H*H/12.0*k2(x+H));
-        y = (fn - x*steig)/.08; //eichung 
-        printf("%g %g %g\n",x,y,fnp1);
-        fnm1=fn;
-	    fn=fnp1;
-    } 
+  // nochmal laufen lassen während man den linearen term abzieht
+  fnm1 = 0.0;
+  fn = H;
+  for (x = H; x <= XMAX; x += H) {
+    fnp1 = numerov(x, fnm1, fn);
+
+    y = (fn - x * steig) / .08; // eichung
+    printf("%g %g %g %g %g\n", x, y, y / x, chi(x),
+           chi(x) /
+               x); // x , Chi_num(x), phi_num(x), chi analytisch, phi analytisch
+
+    fnm1 = fn;
+    fn = fnp1;
+  }
 }
--- P3-Randwertprobleme/shooting.c
+++ P3-Randwertprobleme/shooting.c
@@ -1,62 +1,54 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
-
 
 #define H 0.001
 #define XMAX 20.0
-#define N (XMAX/H)
+#define N (XMAX / H)
 
 double *psi;
-double F(double x){
-    return 0.0;
-}
+double F(double x) { return 0.0; }
 
-double k2(double E,double x) {
-	return x-2*E;
-}
+double k2(double E, double x) { return x - 2 * E; }
 
 double numerov(double E, int save) {
-    double x, fnm1=0.0, fn=H, fnp1, ret=0;
-    double y,df;
-    int i = 0;
+  double x, fnm1 = 0.0, fn = H, fnp1, ret = 0;
+  double y, df;
+  int i = 0;
 
-    //rechte seite
-    for(x=H;x <= XMAX;x += H) {
-		fnp1 = (H*H/12.0*(F(x+H)+10.0*F(x)+F(x-H)) + fn*(2.0-5.0/6.0*H*H*k2(E,x))-fnm1*(1.0+H*H/12.0*k2(E,x-H)))/(1.0+H*H/12.0*k2(E,x+H));
-        if (save ==1){
-            printf("%g %g\n",x,fn);
-        }
-
-        fnm1=fn;
-	    fn=fnp1;
-        psi[i++] = fn;
+  // rechte seite
+  for (x = H; x <= XMAX; x += H) {
+    fnp1 = (H * H / 12.0 * (F(x + H) + 10.0 * F(x) + F(x - H)) +
+            fn * (2.0 - 5.0 / 6.0 * H * H * k2(E, x)) -
+            fnm1 * (1.0 + H * H / 12.0 * k2(E, x - H))) /
+           (1.0 + H * H / 12.0 * k2(E, x + H));
+    if (save == 1) {
+      printf("%g %g\n", x, fn);
     }
 
-    
-    for(int x=N - 100 ;x < N;x++) {
-        // ret += fabs(psi[x]);
-        ret += psi[x];
-        
-    }
+    fnm1 = fn;
+    fn = fnp1;
+    psi[i++] = fn;
+  }
 
-    return ret;
+  for (int x = N - 100; x < N; x++) {
+    // ret += fabs(psi[x]);
+    ret += psi[x];
+  }
+
+  return ret;
 }
 
-int main(){
-    psi = (double *)malloc(2*(int)N*sizeof(double));
-    double E0=0,E1 = 2.,Ex,tmp;
-    // numerov(E1);
-    // printf("%g %g\n", numerov(E1),0.);
-    do {
-		Ex = (E1+E0)/2.0;
-		// bisection (non continous !)
-		tmp = numerov(Ex,0);
-	    if(tmp*numerov(E0,0)>0)
-			E0=Ex;
-	    else
-		    E1=Ex;
-	} while (fabs((E1-E0)/E0)>1.e-14);
-    numerov(Ex,1);
-
+int main() {
+  psi = (double *)malloc(2 * (int)N * sizeof(double));
+  double E0 = 2, E1 = 3, Ex, tmp;
+  do {
+    Ex = (E1 + E0) / 2.0;
+    tmp = numerov(Ex, 0);
+    if (tmp * numerov(E0, 0) > 0)
+      E0 = Ex;
+    else
+      E1 = Ex;
+  } while (fabs((E1 - E0) / E0) > 1.e-14);
+  numerov(Ex, 1);
 }
