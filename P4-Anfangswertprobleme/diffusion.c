2d3fdb47045463211f797a8ede69ff3edfe8414f
emhl <emil@steinr.eu>
2022-07-12 10:36:04 +0200

    fix errors with xpos

--- P4-Anfangswertprobleme/diffusion.c
+++ P4-Anfangswertprobleme/diffusion.c
@@ -4,20 +4,20 @@
 #include <omp.h>
 
 
-#define N 500
-#define T 20	
-#define DT 5.e-3
-#define DX 5.e-3
-#define D 5.e-3
+#define T 2.
+#define DT 2.e-3
+#define DX 1.e-3
+#define D 3.e-3
 #define X0 .5
 #define T0 0.
+#define N 2*X0/DX
 #define SIGMA .02
 #define ALPHA (D*DT/DX/DX)
 
 double *phin, *phinm1;
 
 //werte für t = 0
-double phi_t0(int i){ return exp(-(i*i*DX*DX)/(2*SIGMA*SIGMA));}
+double phi_t0(int i){ return exp(-(i*DX-X0)*(i*DX-X0)/(2*SIGMA*SIGMA));}
 
 //ftcs
 double phi_ftcs(double pm1, double p, double pp1){
@@ -26,16 +26,15 @@ double phi_ftcs(double pm1, double p, double pp1){
 
 int main(){
 double x=X0,t=T0,phi;
-phin=(double *)malloc(2 * (int)N * sizeof(double));
+phin=(double *)malloc((int)N * sizeof(double));
 printf("x t phi \n");
 // t=0
-int j=0;
-while(j<2*N){
-    phi=phi_t0(j-N);
+
+for(int j=0;j<N;j++){
+    phi=phi_t0(j);
     phin[j]= phi;
-    x = X0 + (j-N)*DX;
+    x = j*DX;
     printf("%g %g %g \n", x, t, phi);
-    j++;
 }
 
 //für nächstens schritt implementieren
@@ -45,14 +44,14 @@ while(t<=T){
     phinm1 = phin;
     t += DT;
     phin[0]= phi_ftcs(phinm1[0],phinm1[1],phinm1[2]);
-    phin[2*N-1]= phi_ftcs(phinm1[2*N-3],phinm1[2*N-2],phinm1[2*N-1]);
-    #pragma omp parallel
-        for (int k = 1; k < 2*N-1;k++){
+    phin[(int)(N-1)]= phi_ftcs(phinm1[(int)(N-3)],phinm1[(int)(N-2)],phinm1[(int)(N-1)]);
+    // #pragma omp parallel
+        for (int k = 1; k < N-1;k++){
             phin[k]= phi_ftcs(phinm1[k-1],phinm1[k],phinm1[k+1]);
         }
-    for(int k=0; k < 2*N;k++){
+    for(int k=0; k < N;k++){
         phi = phin[k];
-        x = X0 + (k-N)*DX;
+        x = k*DX;
         printf("%g %g %g \n", x, t, phi);
     }
 }
