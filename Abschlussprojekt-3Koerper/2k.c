1f93938e2a69a62b72f7099fe3a14af55311619a
emhl <emil@steinr.eu>
2022-08-24 17:53:59 +0200

    adapt 2k to 3k and format code

--- Abschlussprojekt-3Koerper/2k.c
+++ Abschlussprojekt-3Koerper/2k.c
@@ -16,7 +16,7 @@
 #define X2 2.
 #define Y2 -1.
 #define VX2 0.
-#define VY2 0.
+#define VY2 .5
 #define M2 4.
 
 // potentielle Energie des Systems
@@ -32,18 +32,16 @@ double ekin(double vx1, double vy1, double vx2, double vy2) {
 #define E epot(X1, X2, Y1, Y2) + ekin(VX1, VY1, VX2, VY2)
 
 // Gravitationskraft zwischen zwei Körpern
-double fg(double r2, double m1, double m2) {
-  return -G * m1 * m2 /r2;
-}
+double fg(double r2, double m1, double m2) { return -G * m1 * m2 / r2; }
 
 double fgx(double x1, double y1, double x2, double y2, double m1, double m2) {
   double r = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-  return fg(r*r, m1, m2) * (x1 - x2) / r;
+  return fg(r * r, m1, m2) * (x1 - x2) / r;
 }
 
 double fgy(double x1, double y1, double x2, double y2, double m1, double m2) {
   double r = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-  return fg(r*r, m1, m2) * (y1 - y2) / r;
+  return fg(r * r, m1, m2) * (y1 - y2) / r;
 }
 
 // Position des Massenzentums des Systems
@@ -56,40 +54,35 @@ double qm(double q1, double q2, double m1, double m2) {
 
 int main() {
 
-double x1 = X1, x2 = X2, y1 = Y1, y2 = Y2, vx1 = VX1, vx2 = VX2, vy1 = VY1, vy2 = VY2;
-double x1n, x2n, y1n, y2n, vx1n, vx2n, vy1n, vy2n;
+  double x1 = X1, x2 = X2, y1 = Y1, y2 = Y2, vx1 = VX1, vx2 = VX2, vy1 = VY1,
+         vy2 = VY2;
+  double x1n, x2n, y1n, y2n, vx1n, vx2n, vy1n, vy2n;
 
-//double x1v = X1, x2v = X2, y1v = Y1, y2v = Y2, vx1v = VX1, vx2v = VX2, vy1v = VY1, vy2v = VY2;
+  for (double t = 0; t < TMAX; t += H) {
 
+    x1n = x1 + vx1 * H + H * H * fgx(x1, y1, x2, y2, M1, M2) / 2 / M1;
+    x2n = x2 + vx2 * H + H * H * fgx(x2, y2, x1, y1, M2, M1) / 2 / M2;
+    y1n = y1 + vy1 * H + H * H * fgy(x1, y1, x2, y2, M1, M2) / 2 / M1;
+    y2n = y2 + vy2 * H + H * H * fgy(x2, y2, x1, y1, M2, M1) / 2 / M2;
 
+    vx1n =
+        vx1 + (fgx(x1, y1, x2, y2, M1, M2) + fgx(x1n, y1n, x2n, y2n, M1, M2)) *
+                  H / 2 / M1;
+    vx2n =
+        vx2 + (fgx(x2, y2, x1, y1, M2, M1) + fgx(x2n, y2n, x1n, y1n, M2, M1)) *
+                  H / 2 / M2;
+    vy1n =
+        vy1 + (fgy(x1, y1, x2, y2, M1, M2) + fgy(x1n, y1n, x2n, y2n, M1, M2)) *
+                  H / 2 / M1;
+    vy2n =
+        vy2 + (fgy(x2, y2, x1, y1, M2, M1) + fgy(x2n, y2n, x1n, y1n, M2, M1)) *
+                  H / 2 / M2;
 
-for (double t = 0; t < TMAX; t += H)
-{
-  
-x1n = x1 + vx1*H + H*H*fgx(x1,y1,x2,y2,M1,M2)/2/M1;
-x2n = x2 + vx2*H + H*H*fgx(x2,y2,x1,y1,M2,M1)/2/M2;
-y1n = y1 + vy1*H + H*H*fgy(x1,y1,x2,y2,M1,M2)/2/M1;
-y2n = y2 + vy2*H + H*H*fgy(x2,y2,x1,y1,M2,M1)/2/M2;
+    printf("%g %g %g %g %g %g %g\n", x1, y1, x2, y2, t,
+           sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))),
+        ekin(vx1, vy1, vx2, vy2) + epot(x1, x2, y1, y2);
 
-vx1n = vx1 + (fgx(x1,y1,x2,y2,M1,M2)+fgx(x1n,y1n,x2n,y2n,M1,M2))*H/2/M1;
-vx2n = vx2 + (fgx(x2,y2,x1,y1,M2,M1)+fgx(x2n,y2n,x1n,y1n,M2,M1))*H/2/M2;
-vy1n = vy1 + (fgy(x1,y1,x2,y2,M1,M2)+fgy(x1n,y1n,x2n,y2n,M1,M2))*H/2/M1;
-vy2n = vy2 + (fgy(x2,y2,x1,y1,M2,M1)+fgy(x2n,y2n,x1n,y1n,M2,M1))*H/2/M2;
-
-
-printf("%g %g %g %g %g %g %g\n", x1, y1, x2, y2, t, sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))), ekin(vx1, vy1, vx2, vy2) + epot(x1, x2, y1, y2);
-
-x1 = x1n;
-x2 = x2n;
-y1 = y1n;
-y2 = y2n;
-vx1 = vx1n;
-vx2 = vx2n;
-vy1 = vy1n;
-vy2 = vy2n;
-
-}
-
-
-
+    x1 = x1n, x2 = x2n, y1 = y1n, y2 = y2n;
+    vx1 = vx1n, vx2 = vx2n, vy1 = vy1n, vy2 = vy2n;
+  }
 }
--- Abschlussprojekt-3Koerper/3k.c
+++ Abschlussprojekt-3Koerper/3k.c
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <stdio.h>
 
+#define TMAX 10
 #define H 1.e-2
 #define G 1.
 
@@ -26,7 +27,7 @@
 #define M3 1
 
 // potentielle Energie des Systems
-double epot(double x1, double x2, double y1, double y2, double x3, double y3) {
+double epot(double x1, double x2, double x3, double y1, double y2, double y3) {
   return -G * M1 * M2 / sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) -
          G * M1 * M3 / sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3)) -
          G * M2 * M3 / sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
@@ -43,8 +44,16 @@ double ekin(double vx1, double vy1, double vx2, double vy2, double vx3,
 #define E epot(X1, X2, Y1, Y2, X3, Y3) + ekin(VX1, VY1, VX2, VY2, VX3, VY3)
 
 // Gravitationskraft zwischen zwei Körpern
-double fg(double q1, double q2, double m1, double m2) {
-  return -G * m1 * m2 / ((q1 - q2) * (q1 - q2));
+double fg(double r2, double m1, double m2) { return -G * m1 * m2 / r2; }
+
+double fgx(double x1, double y1, double x2, double y2, double m1, double m2) {
+  double r = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  return fg(r * r, m1, m2) * (x1 - x2) / r;
+}
+
+double fgy(double x1, double y1, double x2, double y2, double m1, double m2) {
+  double r = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+  return fg(r * r, m1, m2) * (y1 - y2) / r;
 }
 
 // Position des Massenzentums des Systems
@@ -55,4 +64,64 @@ double qm(double q1, double q2, double q3, double m1, double m2, double m3) {
 #define XM qm(X1, X2, X3, M1, M2, M3)
 #define YM qm(Y1, Y2, Y3, M1, M2, M3)
 
-int main() {}
+int main() {
+  double x1 = X1, y1 = Y1, vx1 = VX1, vy1 = VY1;
+  double x2 = X2, y2 = Y2, vx2 = VX2, vy2 = VY2;
+  double x3 = X3, y3 = Y3, vx3 = VX3, vy3 = VY3;
+
+  double x1n, y1n, vx1n, vy1n;
+  double x2n, y2n, vx2n, vy2n;
+  double x3n, y3n, vx3n, vy3n;
+
+  for (double t = 0; t < TMAX; t += H) {
+
+    x1n = x1 + vx1 * H + H * H * fgx(x1, y1, x2, y2, M1, M2) / 2 / M1 +
+          H * H * fgx(x1, y1, x3, y3, M1, M3) / 2 / M1;
+    x2n = x2 + vx2 * H + H * H * fgx(x2, y2, x1, y1, M2, M1) / 2 / M2 +
+          H * H * fgx(x2, y2, x3, y3, M2, M3) / 2 / M2;
+    x3n = x3 + vx3 * H + H * H * fgx(x3, y3, x1, y1, M3, M1) / 2 / M3 +
+          H * H * fgx(x3, y3, x2, y2, M3, M2) / 2 / M3;
+
+    y1n = y1 + vy1 * H + H * H * fgy(x1, y1, x2, y2, M1, M2) / 2 / M1 +
+          H * H * fgy(x1, y1, x3, y3, M1, M3) / 2 / M1;
+    y2n = y2 + vy2 * H + H * H * fgy(x2, y2, x1, y1, M2, M1) / 2 / M2 +
+          H * H * fgy(x2, y2, x3, y3, M2, M3) / 2 / M2;
+    y3n = y3 + vy3 * H + H * H * fgy(x3, y3, x1, y1, M3, M1) / 2 / M3 +
+          H * H * fgy(x3, y3, x2, y2, M3, M2) / 2 / M3;
+
+    vx1n =
+        vx1 + (fgx(x1, y1, x2, y2, M1, M2) + fgx(x1n, y1n, x2n, y2n, M1, M2) +
+               fgx(x1, y1, x3, y3, M1, M3) + fgx(x1n, y1n, x3n, y3n, M1, M3)) /
+                  2 / M1 * H;
+    vx2n =
+        vx2 + (fgx(x2, y2, x1, y1, M2, M1) + fgx(x2n, y2n, x1n, y1n, M2, M1) +
+               fgx(x2, y2, x3, y3, M2, M3) + fgx(x2n, y2n, x3n, y3n, M2, M3)) /
+                  2 / M2 * H;
+    vx3n =
+        vx3 + (fgx(x3, y3, x1, y1, M3, M1) + fgx(x3n, y3n, x1n, y1n, M3, M1) +
+               fgx(x3, y3, x2, y2, M3, M2) + fgx(x3n, y3n, x2n, y2n, M3, M2)) /
+                  2 / M3 * H;
+
+    vy1n =
+        vy1 + (fgy(x1, y1, x2, y2, M1, M2) + fgy(x1n, y1n, x2n, y2n, M1, M2) +
+               fgy(x1, y1, x3, y3, M1, M3) + fgy(x1n, y1n, x3n, y3n, M1, M3)) /
+                  2 / M1 * H;
+    vy2n =
+        vy2 + (fgy(x2, y2, x1, y1, M2, M1) + fgy(x2n, y2n, x1n, y1n, M2, M1) +
+               fgy(x2, y2, x3, y3, M2, M3) + fgy(x2n, y2n, x3n, y3n, M2, M3)) /
+                  2 / M2 * H;
+    vy3n =
+        vy3 + (fgy(x3, y3, x1, y1, M3, M1) + fgy(x3n, y3n, x1n, y1n, M3, M1) +
+               fgy(x3, y3, x2, y2, M3, M2) + fgy(x3n, y3n, x2n, y2n, M3, M2)) /
+                  2 / M3 * H;
+
+    printf("%g %g %g %g %g %g %g %g %g\n", x1, y1, x2, y2, x3, y3, t,
+           sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))),
+        ekin(vx1, vy1, vx2, vy2, vx3, vy3) + epot(x1, x2, x3, y1, y2, y3);
+
+    x1 = x1n, x2 = x2n, x3 = x3n;
+    y1 = y1n, y2 = y2n, y3 = y3n;
+    vx1 = vx1n, vx2 = vx2n, vx3 = vx3n;
+    vy1 = vy1n, vy2 = vy2n, vy3 = vy3n;
+  }
+}
